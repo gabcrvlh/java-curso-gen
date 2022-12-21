c015bfc509891a7e3bc5d51f8dfc60582b3098ca
Gabriela <Gabriela@MacBook-Pro-de-Gabriela.local>
2022-12-21 18:23:15 -0300

    ExercicioDesafioAposta

--- src/com/gabcrvlh/gen/java/Desafios/DesafioAposta.java
+++ src/com/gabcrvlh/gen/java/Desafios/DesafioAposta.java
@@ -11,50 +11,56 @@ public class DesafioAposta {
 
         System.out.println("### Menu da Aposta ###");
 
-        while(true) {
-            if (saldo == 0) {
-                System.out.println("Seu saldo está zerado. Você perdeu.");
-                System.exit(0);
+        while (true) {
+            try {
+                if (saldo <= 0) {
+                    System.out.println("\nSeu saldo está zerado. Você perdeu.");
+                    System.out.println("Agradecemos a participação.");
+                    System.exit(0);
 
-            } else if (saldo >= 200) {
-                System.out.println("Seu saldo atingiu 200 ou mais. Você ganhou!");
-                System.exit(0);
+                } else if (saldo >= 200) {
+                    System.out.println("\nSeu saldo atingiu 200 ou mais. Você ganhou!");
+                    System.out.println("Saldo final: " + saldo);
+                    System.out.println("Agradecemos a participação.");
+                    System.exit(0);
 
-            } else {
-                System.out.println("""
-                \nEscolha um número:
-                1. Ver o saldo atual.
-                2. Parar de jogar e ficar com o saldo atual.
-                3. Continuar jogando.""");
+                } else {
+                    System.out.println("""
+                            \nEscolha um número:
+                            1. Ver o saldo atual.
+                            2. Parar de jogar e ficar com o saldo atual.
+                            3. Continuar jogando.""");
 
-                escolha = read.nextInt();
+                    escolha = Integer.parseInt(read.next());
 
-                switch (escolha) { // testar: switch(read.nextInt()!!)
-                    case 1:
-                        System.out.println("\nSaldo atual: " + saldo);
-                        break;
+                    switch (escolha) {
+                        case 1:
+                            System.out.println("\nSaldo atual: " + saldo);
+                            break;
 
-                    case 2:
-                        System.out.println("Encerrando a aposta...");
-                        System.out.println("Saldo final: " + saldo);
-                        System.out.println("Agradecemos a participação.");
-                        System.exit(0);
+                        case 2:
+                            System.out.println("\nEncerrando a aposta...");
+                            System.out.println("Saldo final: " + saldo);
+                            System.out.println("Agradecemos a participação.");
+                            System.exit(0);
 
 
-                    case 3:
-                        numero = (int) Math.round(Math.random() * 100);
+                        case 3:
+                            numero = (int) Math.round(Math.random() * 100);
 
-                        if (numero < 50) {
-                            saldo -= numero;
+                            if (numero < 50) {
+                                saldo -= numero;
 
-                        } else if (numero == 50) {
-                            continue;
-                        } else {
-                            saldo += numero;
-                        }
-                        break;
-
+                            } else if (numero == 50) {
+                                continue;
+                            } else {
+                                saldo += numero;
+                            }
+                            break;
+                    }
                 }
+            } catch (NumberFormatException e) {
+                System.out.println("Erro: Por favor digite somente números.");
             }
         }
     }
